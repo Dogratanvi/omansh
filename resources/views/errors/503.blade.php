d36ae6272540ea630a1b1b35418995b2212e7d42
SushilKatoch <sushil.katoch@pulseplaydigital.com>
2024-04-16 15:39:53 +0530

    error blade

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,6 @@
-@extends('errors::minimal')
-
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __('Service Unavailable'))
+<x-filament::layouts.card title="Maintenance Mode" class="text-center">
+    <svg class="animate-pulse m-auto h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <x-heroicon-s-heart class="opacity-80" style="color: #14b8a6;" stroke="#14b8a6"></x-heroicon-s-heart>
+    </svg>
+    <p class="mt-4 mb-2">The site is currently down for scheduled maintenance. Check back with us shortly.</p>
+</x-filament::layouts.card>

9499f580343aa8e0b17dd0a6f539925d7b6bb538
SushilKatoch <sushil.katoch@pulseplaydigital.com>
2024-04-16 16:15:51 +0530

    error blade

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -108,9 +108,9 @@ body {
 	<div id="notfound">
 		<div class="notfound">
 			<div class="notfound-404">
-				<h1>4<span>0</span>4</h1>
+				<h1>5<span>0</span>3</h1>
 			</div>
-			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+			<p>The site is currently down for scheduled maintenance. Check back with us shortly.</p>
 		</div>
 	</div>
 

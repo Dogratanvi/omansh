958c1ca0b254e3f318f0a512ce62b7eb339b582d
SushilKatoch <sushil.katoch@pulseplaydigital.com>
2024-05-01 14:57:12 +0530

    seo meta

---
+++ config/meta.php
@@ -0,0 +1,39 @@
+<?php
+return 
+[
+    'home' => [
+        'title' => "OMANSH Health & Fitness - Women's Health, Physiotherapy, Yoga",
+        'description' => "Specialized women's health services, physiotherapy, & rejuvenating yoga sessions at Omansh Health & Fitness.",
+        'keywords' => '',
+        'facebook' => [
+            'title' => "OMANSH Health & Fitness - Women's Health, Physiotherapy, Yoga",
+            'description' => "Specialized women's health services, physiotherapy, & rejuvenating yoga sessions at Omansh Health & Fitness.",
+            'site_name' => 'https://www.omansh.com',
+            'image_name' => 'https://omansh.org/img/logo.png',
+            // Add more Facebook meta data specific to the home page if needed
+        ],
+    ],
+    'about' => [
+        'title' => "OMANSH | Elevating Lives with Holistic Health Solutions",
+        'description' => "Experience personalized physiotherapy, yoga, and women's health care at OMANSH, founded by Dr. Garima Biswas, guiding individuals to vitality and happiness",
+        'keywords' => '',
+        'facebook' => [
+            'title' => "OMANSH | Elevating Lives with Holistic Health Solutions",
+            'description' => "Experience personalized physiotherapy, yoga, and women's health care at OMANSH, founded by Dr. Garima Biswas, guiding individuals to vitality and happiness",
+            'site_name' => 'https://www.omansh.com/',
+            'image_name' => 'https://omansh.org/img/logo.png',
+            // Add more Facebook meta data specific to the home page if needed
+        ],
+    ],
+    'service' => [
+        'title' => "Empowering Women's Health | Physiotherapy | Yoga | OMANSH",
+        'description' => "Elevate women's health with OMANSH-personalized physiotherapy and transformative yoga for holistic well-being and vitality. Discover empowerment today.",
+        'keywords' => '',
+    ],
+    'blog' => [
+        'title' => 'Latest Blog | Wellness During Pregnancy | Yoga and Fitness',
+        'description' => 'Dive into our latest blog, where we explore yoga, fitness, and best pregnancy wellness. Gain expert insights and valuable tips to enrich your journey.',
+        'keywords' => '',
+    ],
+
+];

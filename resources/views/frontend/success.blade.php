99b5f8124e1ce4f90d4bab91fa530f26de2221df
SushilKatoch <sushil.katoch@pulseplaydigital.com>
2026-01-15 17:15:02 +0530

    Update success.blade.php

--- resources/views/frontend/success.blade.php
+++ resources/views/frontend/success.blade.php
@@ -2,7 +2,7 @@
 @extends('frontend.layouts.app')
 @section('content')
 <main class="success-page">
-    <section class="py-5 min-vh-100 d-flex align-items-center">
+    <section class="py-5 min-vh-100 d-flex align-items-center mt-5">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8 col-lg-6">
@@ -108,21 +108,4 @@
      $settings = App\Models\Setting::all();
         $meta_page_type = 'online-rehab-program';
 @endphp
-@section('content')
-<div class="container py-5 mt-5">   {{-- add mt-5 --}}
-    <div class="row justify-content-center">
-        <div class="col-md-8 text-center">
-            <div class="card shadow-lg">
-                <div class="card-body p-5">
-                    <i class="fas fa-check-circle text-success" style="font-size: 80px;"></i>
-                    <h1 class="mt-4 mb-3 text-success">Payment Successful!</h1>
-                    <p class="lead">Thank you for registering for the Diastasis Recti Online Rehab Program.</p>
-                    <p>You will receive a confirmation email shortly with program details.</p>
-                    <hr class="my-4">
-                    <a href="/" class="btn btn-primary btn-lg">Go to Homepage</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+

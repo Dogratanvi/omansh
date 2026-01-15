d895f0e8e0eba858b52d20cae64d21c8b4073a54
SushilKatoch <sushil.katoch@pulseplaydigital.com>
2026-01-15 18:05:14 +0530

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -113,7 +113,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@omansh.org'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
